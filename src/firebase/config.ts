57c80edb8d5b3f839ac8bb55bb14789ce14799be
Vinith Mandadi <vinithmandadi2203995@gmail.com>
2026-02-13 16:30:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6123891357-4ac65",
+  "appId": "1:973374975389:web:498d16d1ba3ec7f4255374",
+  "apiKey": "AIzaSyDo--KLvSNeIkJZY1URijfj38Sh8S6U7xw",
+  "authDomain": "studio-6123891357-4ac65.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "973374975389"
+};
